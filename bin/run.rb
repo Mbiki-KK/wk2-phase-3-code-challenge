1ac77c468bce6832a55a8da3e4c77c985eb0ae3c
Mbiki-KK <wawerumbiki@gmail.com>
2023-05-28 22:40:00 +0300

    added require relative to run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,4 @@
-# require_relative '../lib/article.rb'
-# require_relative '../lib/author.rb' 
-# require_relative '../lib/magazine.rb'
+require_relative '../environment/environment.rb'
 
 # Create authors
 author1 = Author.new("John Doe")
